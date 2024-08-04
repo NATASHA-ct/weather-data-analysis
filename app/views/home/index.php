2ecaff9de6909adc2d43ef5d6dc224bb9ae74e32
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2024-08-04 16:30:41 +0300

    Delete app directory

--- app/views/home/index.php
+++
@@ -1,8 +0,0 @@
-<!-- 
-  HTML page to view index 
-  a $data multidimentional array contains every info that sent to this view 
-  $data['user'] : assosiative array contains user's info
-  and the language translation. 
-  $data['lang']['main'] : main language data
-  $data['lang']['home/index'] : home/index language data
--->
